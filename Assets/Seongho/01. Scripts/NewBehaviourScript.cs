e8091d8638acfaa8cbb4174a384fc6e441d704ce
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2023-03-14 00:35:13 +0900

    적 대강 애니메이션

---
+++ Assets/Seongho/01. Scripts/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Time.timeScale = 0.2f;
+        }
+        else if (Input.GetKeyUp(KeyCode.E))
+            Time.timeScale = 1f;
+    }}
