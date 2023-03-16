fe12246f75d8599232e30a36438c878e5edd04f5
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2023-03-17 08:35:56 +0900

    ..

---
+++ Assets/Seongho/01. Scripts/GunSOData.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SO/GunSOData")]
+public class GunSOData : ScriptableObject
+{
+    public GameObject bullet;
+}
---
+++ Assets/Seongho/01. Scripts/WeaponGun.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponGun : MonoBehaviour
+{
+    [SerializeField]
+    private GunSOData gunData;
+    public void Shooting()
+    {
+        
+    }
+}
