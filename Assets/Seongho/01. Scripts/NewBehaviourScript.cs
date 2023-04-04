7ad4a8820f36a8608f9fb92e2261819130806987
nacfson <nacfson@gmail.com>
2023-04-04 23:10:22 +0900

    dd

--- Assets/Seongho/01. Scripts/NewBehaviourScript.cs
+++ Assets/Seongho/01. Scripts/NewBehaviourScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PoolableMono : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     private void Update()
     {
