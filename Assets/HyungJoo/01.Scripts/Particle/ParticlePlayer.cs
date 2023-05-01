d34bfab08caafd974abc23ab2e7a4066a96de233
nacfson <nacfson@gmail.com>
2023-05-02 00:50:08 +0900

    Asset

--- Assets/HyungJoo/01.Scripts/Particle/ParticlePlayer.cs
+++ Assets/HyungJoo/01.Scripts/Particle/ParticlePlayer.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 using static Core.Define;
 public class ParticlePlayer : PoolableMono{
     private ParticleSystem _particle;
-
-
     private void Awake() {
         _particle = GetComponent<ParticleSystem>();
     }
 
     public override void Init(){
+        Invoke("PoolPush", 2f);
+    }
+    public void PoolPush() {
+        PoolManager.Instance.Push(this);
     }
 }
