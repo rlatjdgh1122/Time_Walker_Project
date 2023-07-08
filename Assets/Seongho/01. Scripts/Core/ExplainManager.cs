92d3287f317603b043f9bdca93b412bce3105d29
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2023-07-08 20:56:06 +0900

    설명씬 끝

--- Assets/Seongho/01. Scripts/Core/ExplainManager.cs
+++ Assets/Seongho/01. Scripts/Core/ExplainManager.cs
@@ -2,58 +2,108 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
+public enum color
+{
+    white,
+    green,
+    yellow,
+    red,
+    blue,
+}
 [System.Serializable]
-public class WriteText
+public class TextInfo
 {
     public string text;
-    public Color textColor = Color.white;
-    public bool isShake = false;
-    public bool isStrong = false;
+    public color color;
+    public int PontSize;
+    public bool isBold;
+    public bool isLb;
 }
-[System.Serializable]
-public class Write
-{
-    public List<WriteText> texts = new List<WriteText>();
-}
+
 public class ExplainManager : MonoBehaviour
 {
-    public List<Write> pages = new List<Write>();
-    public TextMeshProUGUI _text;
+    public List<TextInfo> textInfos = new();
+
+    [SerializeField]
+    private TMP_Text _tmpText;
+
+    [SerializeField]
+    private float _oneCharacterTime = 0.2f;
+
+    private bool _isType = false;  //타이핑중인지 확인
+    private string resultText;
     private void Start()
     {
-        ReadingStart();
-    }
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space)) StopAllCoroutines();
-    }
-    public void ReadingStart()
-    {
-        StartCoroutine(Reading());
-    }
-    private IEnumerator Reading()
-    {
-        for (int i = 0; i < pages.Count; i++)
-        {
-            yield return StartCoroutine((ReadPage(pages[i])));
-        }
-    }
-    private IEnumerator ReadPage(Write write)
-    {
-        for (int i = 0; i < write.texts.Count; i++)
-        {
-            yield return StartCoroutine((PrintText(write.texts[i].text)));
-        }
-    }
-    private IEnumerator PrintText(string text)
-    {
-        for (int i = 0; i < text.Length; i++)
-        {
-            _text.text += text[i];
-            yield return new WaitForSeconds(.1f);
-        }
+        PostProcessingController.Instance.StopAllCoroutine();
 
+        PostProcessingController.Instance.Set_LensDistortion(1, .3f, .9f);
+        PostProcessingController.Instance.Set_DigitalGlitchVolume(1, 0, 1);
+
+        for (int i = 0; i < textInfos.Count; i++)
+            Write(textInfos[i]);
+
+        Invoke("StartEffect", 1.2f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StopAllCoroutines();
+        }
+    }
+    private void Write(TextInfo textInfo)
+    {
+        string text = "";
+        text += $"<size={textInfo.PontSize}>";
+        text += $"<color={textInfo.color.ToString()}>";
+        if (textInfo.isBold)
+        {
+            text += "<b>";
+        }
+        text += textInfo.text;
+        if (textInfo.isBold)
+        {
+            text += "</b>";
+        }
+        if (textInfo.isLb)
+            text += "\n";
+        text += $"</color>";
+        text += "</size>";
+
+        resultText += text;
+    }
+    private void StartEffect()
+    {
+        _tmpText.SetText(resultText);
+        _tmpText.ForceMeshUpdate();
+        _tmpText.maxVisibleCharacters = 0;
+        StartCoroutine(TypeText());
+    }
+
+    private IEnumerator TypeText()
+    {
+        TMP_TextInfo textInfo = _tmpText.textInfo;
+        for (int i = 0; i < textInfo.characterCount; ++i)
+        {
+            yield return StartCoroutine(TypeOneCharCoroutine(textInfo, i));
+        }
+    }
+
+    private IEnumerator TypeOneCharCoroutine(TMP_TextInfo textInfo, int idx)
+    {
+        _tmpText.maxVisibleCharacters++;
+        _tmpText.ForceMeshUpdate();
+
+        TMP_CharacterInfo charInfo = textInfo.characterInfo[idx];
+        if (charInfo.isVisible == false)
+        {
+            yield return new WaitForSeconds(_oneCharacterTime);
+        }
+        else
+        {
+            yield return null;
+        }
     }
 }
